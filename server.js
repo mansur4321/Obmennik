d1030383f4f76923e411051603ab961f73e4622e
mansur4321 <33467207+mansur4321@users.noreply.github.com>
2024-10-16 03:39:08 +0300

    add gitignore and tgBot refLink logic

--- server.js
+++ server.js
@@ -2,7 +2,95 @@ const http = require("http");
 const fs = require("fs");
 const querystring = require("node:querystring");
 
-const API_KEY = "076f5977a4bd8b19590b0b61a0912acfe373f3830800c1eaa638c222354338cd";
+const TelegramBot = require("node-telegram-bot-api");
+
+const CRYPTO_API_KEY = "076f5977a4bd8b19590b0b61a0912acfe373f3830800c1eaa638c222354338cd";
+const TG_API_KEY = "8045675354:AAHoNUt297syjuvwT7HOa-rHIjoQjchVG34";
+
+const commands = [
+	{
+		command: "start",
+		description: "Запуск бота",
+	},
+	{
+		command: "ref",
+		description: "Получить реферальную ссылку",
+	},
+];
+
+let usersRefsMap = new Map();
+
+function addUser(id, name) {
+	if (usersRefsMap.has(id)) return;
+
+	usersRefsMap.set(id, {
+		userID: id,
+		userName: name,
+		referrers: [],
+	});
+
+	console.log(usersRefsMap);
+}
+
+function addReferrer(userId, referrerId, referrerName) {
+	if (!usersRefsMap.has(userId)) return;
+
+	const userData = usersRefsMap.get(userId);
+
+	if (userData.referrers.find((data) => data.referrerID === referrerId)) return;
+
+	usersRefsMap.set(userId, {
+		userName: userData.userName,
+		userID: userData.userID,
+		referrers: [
+			...userData.referrers,
+			{
+				referrerID: referrerId,
+				referrerName: referrerName,
+			},
+		],
+	});
+
+	console.log(usersRefsMap, usersRefsMap.get(userId));
+}
+
+const bot = new TelegramBot(TG_API_KEY, {
+	polling: {
+		interval: 300,
+		autoStart: true,
+	},
+});
+
+bot.setMyCommands(commands);
+
+bot.onText(/\/start(.*)/, async (msg, match) => {
+	console.log("start", msg.text, msg);
+	const chatId = msg.chat.id;
+	const referrer = match[1] ? match[1].trim() : "";
+	console.log("referrer: ", referrer, "!");
+	try {
+		addUser(msg.from.id, msg.from.username);
+		await bot.sendMessage(chatId, `Вы запустили бота!`);
+
+		if (!referrer) return;
+
+		addReferrer(referrer, msg.from.id, msg.from.username);
+		await bot.sendMessage(chatId, `Вы пришли по реферальной ссылке: ${referrer}`);
+	} catch (err) {
+		console.log(err);
+		await bot.sendMessage(chatId, msg.text + " - не понял команду я твою, Лох");
+	}
+});
+bot.onText(/\/ref/, async (msg) => {
+	console.log("ref", msg.text, msg);
+	const chatId = msg.chat.id;
+
+	try {
+		await bot.sendMessage(chatId, `Ваша реферальная ссылка - ${"https://t.me/Flichain_bot"}?start=${msg.from.id}`);
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 const server = http
 	.createServer(async (request, response) => {
@@ -79,7 +167,7 @@ const server = http
 
 			if (request.url.startsWith("/get-currency-range")) {
 				const { from, to } = querystring.parse(request.url.split("?")[1]);
-				const api = `https://api.changenow.io/v1/exchange-range/${from}_${to}?api_key=${API_KEY}`;
+				const api = `https://api.changenow.io/v1/exchange-range/${from}_${to}?CRYPTO_API_KEY=${CRYPTO_API_KEY}`;
 
 				try {
 					const resp = await fetch(api);
@@ -106,7 +194,7 @@ const server = http
 
 			if (request.url.startsWith("/get-estimated-exchange-amount")) {
 				const { amount, from, to } = querystring.parse(request.url.split("?")[1]);
-				const api = `https://api.changenow.io/v1/exchange-amount/${amount}/${from}_${to}?api_key=${API_KEY}`;
+				const api = `https://api.changenow.io/v1/exchange-amount/${amount}/${from}_${to}?CRYPTO_API_KEY=${CRYPTO_API_KEY}`;
 
 				try {
 					const resp = await fetch(api);
@@ -132,7 +220,7 @@ const server = http
 
 			if (request.url.startsWith("/get-transaction-status")) {
 				const { id } = querystring.parse(request.url.split("?")[1]);
-				const api = `https://api.changenow.io/v1/transactions/${id}/${API_KEY}`;
+				const api = `https://api.changenow.io/v1/transactions/${id}/${CRYPTO_API_KEY}`;
 
 				try {
 					const resp = await fetch(api);
@@ -167,7 +255,7 @@ const server = http
 				request.on("end", async () => {
 					const { from, to, address, amount } = await JSON.parse(data);
 
-					const api = `https://api.changenow.io/v1/transactions/${API_KEY}`;
+					const api = `https://api.changenow.io/v1/transactions/${CRYPTO_API_KEY}`;
 					const body = {
 						from: from,
 						to: to,
@@ -212,6 +300,6 @@ const server = http
 			}
 		}
 	})
-	.listen(3000, function () {
-		console.log("SERVER HOST - http://localhost:3000");
+	.listen(80, function () {
+		console.log("SERVER HOST - http://localhost:8080");
 	});
